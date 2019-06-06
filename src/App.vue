45b494550ed3498a2866f7168864dd58d0d3833a
dadong <1069052661@qq.com>
2019-06-06 11:51:55 +0800

    项目搭建

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <keep-alive>
+         <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+<script>
+  export default {
+    name:"App",
+    components:{
+    
+    }
+  }
+</script>
+
+
+
+
+<style>
+#app{
+  height: 100%;
+}
+</style>
